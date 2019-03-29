74d9bcd751b6084fb79a5c786b2e4b00c0820da7
wangguangya60 <1173799803@qq.com>
2019-03-29 18:08:03 +0800

    search for tandem repeats or dinucleotides
    search for tandem repeats or dinucleotides in genome

--- Pyscripts/findSimpleRegions_quad.sh
+++ Pyscripts/findSimpleRegions_quad.sh
@@ -1,1 +1,1 @@
-python ./findSimpleRegions_quad.py -p 110 -d 100000 -t 100000 -q 100000  hg19.fasta SimpleRepeat_polyA.bed
+python ./findSimpleRegions_quad.py -p 110 -d 100000 -t 100000 -q 100000  hg19.fasta SimpleRepeat_polyA.bed 
