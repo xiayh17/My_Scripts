c6817467622d19b7bd1fc64a73bf3daed7d8bf9d
wangguangya60 <1173799803@qq.com>
2020-06-24 14:04:16 +0800

    Update stacked_barplot_text.R

--- Rscripts/stacked_barplot/stacked_barplot_text.R
+++ Rscripts/stacked_barplot/stacked_barplot_text.R
@@ -2,7 +2,7 @@ require(ggplot2)
 require(reshape2)
 require(dplyr)
 COLORS<- c("#310B68","#FC6B6B","#6BCC69","#C59E37","#FECE68","#CB3535","#036903","#FE0405","#389C66","#FC9D05",
-"#073435","#386A9D","#FF9A9B","#CD0438","#04CC06","#060436","#    D2686B","#373599","#37CD05","#D29BD1")
+"#073435","#386A9D","#FF9A9B","#CD0438","#04CC06","#060436","#D2686B","#373599","#37CD05","#D29BD1")
 df <- read.table('snp.opt-1.barplot.txt',sep="\t",header = T)
 df2 <- melt(df, id=c("gc_bin"))
 text <- df2 %>% group_by(variable) %>% summarize(total = sum(value))     ## total number of each varable
